e82fa95e1116e04c4591981165263732e5be551c
NaotoSG <chichiko3211@gmail.com>
2019-12-19 23:09:06 +0900

    add class to model, and changed message in controller and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def index
-    @greet = "Hello World!!"
+    @message = "抽選結果を表示します！！！"
+
+    user = User.new
+
+    @my_result = user.result
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User
+  def initialize
+    @name = %w(なおとSG こうた たろう Andew のぞみ Alex Brian Daniel)
+    @price = %w(10円 100円 1000円 100万円 1億円)
+  end
+  def result
+    <<~EOS
+    -----------------------------------------
+    おめでとうございます。
+    当選者は#{@name.sample}です！
+    当選金額は#{@price.sample}です。
+    -----------------------------------------
+    EOS
+  end
 end
